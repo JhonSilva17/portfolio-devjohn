f45cf6adcace811540ddb28869cbbd542c916a61
Jhon <jhon.dev.silva@gmail.com>
2021-07-09 16:47:38 -0300

    add library ScrollReveal

--- public/js/script.js
+++ public/js/script.js
@@ -8,19 +8,33 @@ const Menu = {
     }
 }
 
-function scrollMenu() {
-    const header = document.querySelector('#header')
-    const intro = document.querySelector('#intro')
-    const ativo = "ativo"
+// Quando eu der scroll até determinada parte do site, o menu aparece
+const header = document.querySelector('#header')
+const intro = document.querySelector('#intro')
+const ativo = "ativo"
 
-    function surgirMenu() {
-        const medidasIntro = intro.getBoundingClientRect().bottom
-        if (medidasIntro < 0) {
-            header.classList.add(ativo)
-        } else {
-            header.classList.remove(ativo)
-        }
+function surgirMenu() {
+    const medidasIntro = intro.getBoundingClientRect().bottom
+    if (medidasIntro < 0) {
+        header.classList.add(ativo)
+    } else {
+        header.classList.remove(ativo)
     }
-    window.addEventListener('scroll', surgirMenu)
 }
-scrollMenu()
+window.addEventListener('scroll', surgirMenu)
+
+/* ScrollReveal - Quando eu der scroll no site, eu consigo animar os itens */
+const scrollReveal = ScrollReveal({
+    origin: top,
+    distance: '20px',
+    duration: 500,
+    reset: true,
+})
+
+scrollReveal.reveal(`
+    #intro, #intro h1,
+    #projects, #projects h2, #projects .init-p, #projects .container-card,
+    #about, #about img, #about p, #about ul, #about h3,
+    #contact h2, #contact p, #contact label, #contact input, #contact textarea, #contact button, 
+    #footer, #footer .logo, #footer ul li
+`, {interval: 90})
